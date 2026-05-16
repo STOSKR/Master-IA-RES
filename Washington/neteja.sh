5c6da09d0c461b749bb3951a8e8fdf6ef86d68f7
Shiyi <scheng1@etsinf.upv.es>
2026-05-16 16:04:41 +0200

    Datos

--- Washington/neteja.sh
+++ Washington/neteja.sh
@@ -7,19 +7,29 @@ fi
 
 set -e
 
-DOUT=$2
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+IMGTXTENH_DIR="${SCRIPT_DIR}/imgtxtenh"
+MAGICK6_RUNTIME="${IMGTXTENH_DIR}/runtime"
 
-[ -e $1 ] || { echo "$1 doesn't exist ..."; exit 1; }
-[ -d $DOUT ] || mkdir $DOUT
+if [ -d "${MAGICK6_RUNTIME}/lib" ]; then
+  export LD_LIBRARY_PATH="${MAGICK6_RUNTIME}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+  export MAGICK_CONFIGURE_PATH="${MAGICK6_RUNTIME}/etc/ImageMagick-6${MAGICK_CONFIGURE_PATH:+:${MAGICK_CONFIGURE_PATH}}"
+  export MAGICK_CODER_MODULE_PATH="${MAGICK6_RUNTIME}/lib/ImageMagick-6.9.12/modules-Q16/coders${MAGICK_CODER_MODULE_PATH:+:${MAGICK_CODER_MODULE_PATH}}"
+fi
 
-for file in `cat $1`; do 
+DOUT="$2"
+
+[ -e "$1" ] || { echo "$1 doesn't exist ..."; exit 1; }
+[ -d "$DOUT" ] || mkdir -p "$DOUT"
+
+while IFS= read -r file; do
+   [ -n "$file" ] || continue
    echo "Processing $file"
 
-   EXT=`head -1 $1 |awk -F "." '{print $NF}'`
-   N=`basename $file .$EXT`
-   NOM=`echo -e ${DOUT}"/"${N}`
+   EXT="${file##*.}"
+   N="$(basename "$file" ".$EXT")"
+   NOM="${DOUT}/${N}"
 
-  ./imgtxtenh/imgtxtenh   -w 45 -s 0.2 -k 0.3  $file  $NOM.jpg 
+  "${IMGTXTENH_DIR}/imgtxtenh"   -w 45 -s 0.2 -k 0.3  "$file"  "$NOM.jpg"
 
-done
-
+done < "$1"
